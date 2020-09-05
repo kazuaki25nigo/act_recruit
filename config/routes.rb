d7fd917c5d6ebe831be63e0f992a9bf79cee6b4c
sega16322 <aventador_849@docomo.ne.jp>
2020-09-05 13:38:25 +0900

    メーラー・モデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'inquiry/index'    # 入力画面
+  get 'inquiry/confirm'  # 確認画面
+  get 'inquiry/thanks'   # 送信完了画面
   root to: "tops#index"
   resources :new_graduates, only: [:index]
 end
