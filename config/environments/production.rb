f2b30f1c1621100aded97606f3d2449ecf870306
sega16322 <aventador_849@docomo.ne.jp>
2020-09-10 21:38:39 +0900

    メーラーの詳細設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,12 @@ Rails.application.configure do
   # Setup the mailer config
   # Use SendGrid - Add-ons - Heroku
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: '独自ドメイン' }
+  config.action_mailer.default_url_options = { host: 'actool.jp' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
       password: ENV['SENDGRID_PASSWORD'],
-      domain: '独自ドメイン',
+      domain: 'actool.jp',
       address: 'smtp.sendgrid.net',
       port: 587,
       authentication: :plain,
